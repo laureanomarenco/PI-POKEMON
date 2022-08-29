f21bc7eac9872493148b8a8a54b841d1ec16c5bd
laureanomarenco <laureanomarenco@gmail.com>
2022-08-29 14:47:50 -0300

    v.1.0

--- api/tests/models/pokemon.spec.js
+++ api/tests/models/pokemon.spec.js
@@ -1,4 +1,4 @@
-const { Pokemon, conn } = require('../../src/db.js');
+const { Pokemons, conn } = require('../../src/db.js');
 const { expect } = require('chai');
 
 describe('Pokemon model', () => {
@@ -7,15 +7,42 @@ describe('Pokemon model', () => {
       console.error('Unable to connect to the database:', err);
     }));
   describe('Validators', () => {
-    beforeEach(() => Pokemon.sync({ force: true }));
+    beforeEach(() => Pokemons.sync({ force: true }));
     describe('name', () => {
       it('should throw an error if name is null', (done) => {
-        Pokemon.create({})
+        Pokemons.create({})
           .then(() => done(new Error('It requires a valid name')))
           .catch(() => done());
       });
       it('should work when its a valid name', () => {
-        Pokemon.create({ name: 'Pikachu' });
+        Pokemons.create({ name: 'Pikachu' });
+      });});
+    describe('all properties', () => {
+      it('should work when pass a valid complete pokemon', () => {
+        Pokemons.create({ 
+          name: 'Pikachu',
+          hp: 35,
+          attack: 20,
+          defense: 18,
+          velocity: 36,
+          height: 13,
+          weight: 10,
+          imageDefault: 'https://assets.puzzlefactory.pl/puzzle/254/191/original.jpg'
+       });
+      });
+      it('should throw an error if some value is incorrect', (done) => {
+        Pokemons.create({
+          name: 'Pikachu',
+          hp: 35,
+          attack: 20,
+          defense: 18,
+          velocity: 'string',
+          height: 13,
+          weight: 10,
+          imageDefault: 'https://assets.puzzlefactory.pl/puzzle/254/191/original.jpg'
+        })
+          .then(() => done(new Error('It requires a valid name')))
+          .catch(() => done());
       });
     });
   });
