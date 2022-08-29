e45015a159f1a300c852be4b30b53261c5696d02
laureanomarenco <laureanomarenco@gmail.com>
2022-08-29 19:13:21 -0300

    v1.0

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import Home from "./components/home/home";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test("renders the app", () => {
+  render(
+    <Router>
+      <App />
+    </Router>
+  );
+  const linkElement = screen.getByText(/laureano marenco/i);
   expect(linkElement).toBeInTheDocument();
 });
